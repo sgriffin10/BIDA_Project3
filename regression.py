d79a7d2f0ef46b40612ab43639e6e9e25ac03f47
sgriffin10 <sgriffin4@babson.edu>
2019-12-14 20:47:39 -0500

    adding models

--- regression.py
+++ regression.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import KFold
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -35,31 +36,40 @@ df = pd.read_csv("US_Accidents_May19_cleaned_noNAs_1.csv")
 # print(df.feature_names)
 
 # Split the train and test data sets
-# X_train, X_test, y_train, y_test = train_test_split(df, test_size = 0.25)
-train, test, = train_test_split(df, test_size = 0.25)
+# X = pd.DataFrame(df["Temperature(F)"])
+# y = pd.DataFrame(df["Severity"])
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+# train, test = train_test_split(df, test_size = 0.25)
 
-# Instantiate the LinearRegression estimator
-#"Start_Time","Side","City","County","State","Zipcode","Timezone"
+
+# # Instantiate the LinearRegression estimator
+# #"Start_Time","Side","City","County","State","Zipcode","Timezone"
 variables_list = ["Start_Lat","Distance(mi)","Temperature(F)","Wind_Chill(F)"]
+model_list = [LinearRegression(),SGDRegressor()]
 
 for column in variables_list:
-    model = LinearRegression();
-    model.fit(X= pd.DataFrame(df[column]), y = pd.DataFrame(df["Severity"]));
-    # print(model.intercept_)
-    # for i, column in df.columns[:]:
-    print(column, model.coef_)
-    # print(f'{column:>10}: {model.coef_[i]}')
-    # predicted = model.predict((X_test)) * 1000
-    # expected = y_test*1000
-
-
-# # Accuracy
-
-# print(metrics.r2_score(expected, predicted))
-# print(metrics.mean_absolute_error(expected, predicted))
-# print(metrics.mean_squared_error(expected, predicted))
-# print(np.sqrt(metrics.mean_squared_error(expected, predicted)))
-# print(np.sqrt(metrics.mean_squared_log_error(expected, predicted)))
+    X = pd.DataFrame(df[column])
+    y = pd.DataFrame(df["Severity"])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+    for models in model_list:
+        model = models;
+        model.fit(X = X_train, y = y_train);
+        # model.fit(X = pd.DataFrame(df[column]), y = pd.DataFrame(df["Severity"]));
+        # print(model.intercept_)
+        # for i, column in df.columns[:]:
+        print("\u0332".join(column))
+        # print()
+        print("Coefficient:", model.coef_)
+        # print(f'{column:>10}: {model.coef_[i]}')
+        predicted = model.predict((X_test))  
+        expected = y_test
+        # # Accuracy
+        print("R-Squared Score: ", metrics.r2_score(expected, predicted))
+        print("Mean Absolute Error: ", metrics.mean_absolute_error(expected, predicted))
+        print("Mean Squared Error: ", metrics.mean_squared_error(expected, predicted))
+        print("Root Mean Squared Error: ", np.sqrt(metrics.mean_squared_error(expected, predicted)))
+        print("Mean Squared Log Error: ", np.sqrt(metrics.mean_squared_log_error(expected, predicted)))
+        print()
 
 # # ---visualization
 
