00bc39a63188c664acb7fbfb90c17349d59fbf8b
sgriffin10 <sgriffin4@babson.edu>
2019-12-15 18:00:43 -0500

    changes

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,26 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv("car_insurance_claim.csv", sep = ',') #reads csv
-print(df) #check if prints
-
-# df = df.drop(df.columns[32:49], axis = 1)
-
-
-# #### Cleaning Code #####
-df = df.drop(columns=["ID","BIRTH","HOMEKIDS","TIF","RED_CAR"]) #drops the 6 useless columns
-print(df)
-# df.to_csv("Final_Project/Data/US_Accidents_May19_cleaned_2.csv")
-
-# count = 0 
-# for column in df.columns[:]: 
-#     if df[column].isnull().any() == True: 
+#Import CSV to dataframe
+df = pd.read_csv("car_insurance_claim.csv", sep = ',') 
+# print(df) 
 
 
-df = df.dropna() #if you want to remove all rows with missing values
-print(df.isnull().any()) #check
-# print(df) #visual of df after dropped rows
 
-# print(df.types)
+# Drop Irrelevant Columns 
+df = df.drop(columns=["ID","BIRTH","HOMEKIDS","TIF","RED_CAR"]) 
+# print(df)
+
+#Drops rows with missing values
+df = df.dropna() 
+print(df.isnull().any()) 
+# print(df) 
+
+#Removes "$" and "," from rows
 for column in df.columns[:]:
     df[column] = df[column].astype(str) 
     df[column] = df[column].str.replace('$', '')
@@ -29,30 +24,28 @@ for column in df.columns[:]:
 # print(df.dtypes)
 
 
-#Creating Dummy Variables
+#Creates Dummy Variables (either 0 or 1) for variables with only 2 choices
 dummy_req_list = ["PARENT1","MSTATUS","GENDER","CAR_USE","REVOKED","URBANICITY"]
 for column in dummy_req_list:
     df[column] = pd.get_dummies(df[column])
 # print(df)
 # print(df.dtypes)
 
-#Creating Dummy Variables
+#Creates Categorical Variables (0,1,....9,10,etc.)
 cat_req_list = ["EDUCATION","OCCUPATION","CAR_TYPE"]
 for column in cat_req_list:
     df[column] = df[column].astype('category')
     df[column] = df[column].cat.codes
-print(df)
+# print(df)
 # print(df.dtypes)
 
-# Change object to int
+# Changes Data Types from Object to Int
 for column in df.columns[:]:
     if df[column].dtypes == "object":
         # df[column] = df[column].astype(str) 
         df[column] = df[column].astype(float)
-print(df) 
-print(df.dtypes)
+# print(df) 
+# print(df.dtypes)
 
-
-
-
+#Saves to a new file
 df.to_csv("car_insurance_claim_noNAs_2.csv")
--- regression.py
+++ regression.py
@@ -1,57 +1,54 @@
-from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
-from sklearn.model_selection import KFold
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import numpy as np
 
-df = pd.read_csv("car_insurance_claim_noNAs_2.csv")
-
-# print(df.describe())
-# print(df.shape)
-# print(df.dtypes)
-
-model_list = [LinearRegression()] #,SGDRegressor()
-empty_list = []
-for column in df.columns[1:19]:
-    empty_list.append(column)
-for column in df.columns[20:23]:
-        empty_list.append(column)
-# print(empty_list)
+def regression_model(dataframe, set_list, list_of_models):
     
+    #Creates Testing/Training Sets
+    X = pd.DataFrame(dataframe[set_list]) 
+    y = pd.DataFrame(dataframe["CLM_AMT"]) 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=1) 
 
-
-human_list = ["AGE", "YOJ", "INCOME", "PARENT1", "HOME_VAL", "MSTATUS", "GENDER", "EDUCATION", "OCCUPATION"]
-
-#Spencer Comment: Not sure what regression parameters are????
-def regression_model(a_list):
-# for column in variables_list: #runs through integer variables list above
-    X = pd.DataFrame(df[a_list]) #Whatever is in the list above
-    y = pd.DataFrame(df["CLM_AMT"]) #Severity is dependent variable
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=1) #25/75 test-training split
-    for models in model_list: #loops through the 2 models in list above
+    #Model Loop
+    for models in list_of_models: 
         model = models;
-        model.fit(X = X_train, y = y_train);
+        model.fit(X = X_train, y = y_train.values.ravel());
         # print(model.intercept_) #prints intercept
-        # print("\u0332".join(human_list)) #prints underline
         np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
-        print("Coefficient:", model.coef_) #prints coefficient
+        print("Coefficient:", model.coef_) 
         predicted = model.predict((X_test))  
         expected = y_test
+        
         ### Accuracy Scores ###
-        # Spencer Comment: Will need to assign the below to variables and then print
         print("R-Squared Score: ", metrics.r2_score(expected, predicted))
         print("Mean Absolute Error: ", metrics.mean_absolute_error(expected, predicted))
         print("Mean Squared Error: ", metrics.mean_squared_error(expected, predicted))
         print("Root Mean Squared Error: ", np.sqrt(metrics.mean_squared_error(expected, predicted)))
         # print("Mean Squared Log Error: ", np.sqrt(metrics.mean_squared_log_error(expected, predicted)))
-        print() #prints space
+        print() 
 
 def main():
-    regression_model(empty_list)
+
+    #Reads CSV into dataframe
+    df = pd.read_csv("car_insurance_claim_noNAs_2.csv")
+
+    #Creating List of Columns
+    entire_list = []
+    for column in df.columns[1:19]:
+        entire_list.append(column)
+    for column in df.columns[20:23]:
+            entire_list.append(column)
+
+    #List of Models
+    model_list = [LinearRegression(),SGDRegressor(loss="squared_loss",penalty=None,eta0=0.00000000015)]
+
+    #Calls Regresson Model 
+    regression_model(df, entire_list, model_list)
+    
 
 if __name__ == "__main__":
     main()
