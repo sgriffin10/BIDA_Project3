43870d60efc7650d1a48e3785f58560c6178ee5f
sgriffin10 <sgriffin4@babson.edu>
2019-12-15 22:21:02 -0500

    demo code change

--- demo.py
+++ demo.py
@@ -20,66 +20,45 @@ for column in df.columns[20:23]:
 subset_list_human_char = ["KIDSDRIV", "AGE", "YOJ", "INCOME", "PARENT1", "HOME_VAL", "MSTATUS", "GENDER", "EDUCATION", "OCCUPATION"]
 subset_list_car_char = ["TRAVTIME", "CAR_USE", "BLUEBOOK", "CAR_TYPE", "OLDCLAIM", "CLM_FREQ", "REVOKED", "MVR_PTS", "CAR_AGE", "CLAIM_FLAG", "URBANICITY"]
 
+entire_thing = [entire_list, subset_list_human_char, subset_list_car_char]
+# print(entire_thing)
+
 #List of Models
 model_list = [LinearRegression(),SGDRegressor(eta0=0.000000004)]
 # play w. parameters
 # explain that we tried the best we can; but that we could explain it at some later time
 # share the original & clean code 
 
+for a_list in entire_thing:
+    X_train, X_test, y_train, y_test = train_test_split(df[entire_thing], df["CLM_AMT"], test_size = 0.25, random_state=1) 
+    #Model Loop
+    for models in model_list: 
+        model = models;
+        model.fit(X = X_train, y = y_train.values.ravel());
+        # print(model.intercept_) #prints intercept
+        np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
+        print("Coefficient:", model.coef_) 
+        predicted = model.predict((X_test))  
+        expected = y_test
+        
+        ### Accuracy Scores ###
+        print("R-Squared Score: ", metrics.r2_score(expected, predicted))
+        print("Mean Absolute Error: ", metrics.mean_absolute_error(expected, predicted))
+        print("Mean Squared Error: ", metrics.mean_squared_error(expected, predicted))
+        print("Root Mean Squared Error: ", np.sqrt(metrics.mean_squared_error(expected, predicted)))
+        # print("Mean Squared Log Error: ", np.sqrt(metrics.mean_squared_log_error(expected + 1286, predicted + 1286)))#logarithm brings the points closer ; look at it in a graph
+        print() 
 
-#Calls Regresson Model 
-# regression_model(df, entire_list, model_list)
-# regression_model(df, subset_list_human_char, model_list)
-# regression_model(df, subset_list_car_char, model_list)
-
-# linear_reg_visusalization(df, entire_list, model_list)
-
-    
-#Creates Testing/Training Sets
-# X = pd.DataFrame(dataframe[entire_list]) 
-# y = pd.DataFrame(dataframe["CLM_AMT"]) 
-X_train, X_test, y_train, y_test = train_test_split(df[entire_list], df["CLM_AMT"], test_size = 0.25, random_state=1) 
-
-#Model Loop
-for models in model_list: 
-    model = models;
-    model.fit(X = X_train, y = y_train.values.ravel());
-    # print(model.intercept_) #prints intercept
-    np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
-    print("Coefficient:", model.coef_) 
-    predicted = model.predict((X_test))  
-    expected = y_test
-    
-    ### Accuracy Scores ###
-    print("R-Squared Score: ", metrics.r2_score(expected, predicted))
-    print("Mean Absolute Error: ", metrics.mean_absolute_error(expected, predicted))
-    print("Mean Squared Error: ", metrics.mean_squared_error(expected, predicted))
-    print("Root Mean Squared Error: ", np.sqrt(metrics.mean_squared_error(expected, predicted)))
-    # print("Mean Squared Log Error: ", np.sqrt(metrics.mean_squared_log_error(expected + 1286, predicted + 1286)))#logarithm brings the points closer ; look at it in a graph
-    print() 
-
-    df = pd.DataFrame()
-    df['Expected'] = pd.Series(expected)
-    df['Predicted'] = pd.Series(predicted)
-    # font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal', 'size': 12, }
-    plt.figure()
-    plt.scatter(df['Expected'], df['Predicted'], alpha=0.5)
-    plt.title('Linear Regression Scatter Plot')
-    plt.xlabel('Expected')
-    plt.ylabel('Predicted')
-    plt.text(5000, 100, "r-squared value: " + str(metrics.r2_score(expected, predicted)))
-    plt.text(5000, 200, "mean-squared error: " + str(metrics.mean_squared_error(expected, predicted)))
-    plt.figure()
-    plt.show()
-    #     plt.scatter(df['Expected'], df['Predicted2'], alpha=0.5)
-    #     plt.title('SGD Regressor')
-    #     plt.xlabel('Expected')
-    #     plt.ylabel('Predicted2')
-    #     plt.text(5000, 100, "r-squared value: " + str(metrics.r2_score(expected, predicted2)), fontdict=font)
-    #     plt.text(5000, 200, "mean-squared error: " + str(metrics.mean_squared_error(expected, predicted2)), fontdict=font)
-    #     plt.show()
-
-
-
-
-
+        df = pd.DataFrame()
+        df['Expected'] = pd.Series(expected)
+        df['Predicted'] = pd.Series(predicted)
+        # font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal', 'size': 12, }
+        plt.figure()
+        plt.scatter(df['Expected'], df['Predicted'], alpha=0.5)
+        plt.title('Linear Regression Scatter Plot')
+        plt.xlabel('Expected')
+        plt.ylabel('Predicted')
+        plt.text(5000, 100, "r-squared value: " + str(metrics.r2_score(expected, predicted)))
+        plt.text(5000, 500, "mean-squared error: " + str(metrics.mean_squared_error(expected, predicted)))
+        plt.show()
+        df = pd.read_csv("car_insurance_claim_noNAs_2.csv")
